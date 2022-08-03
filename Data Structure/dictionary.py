783cf55cfc207d4b7cf250f74be48928a500fa4d
osaidAthar <osaidathar786633270@gmail.com>
2022-08-03 14:42:51 +0530

    dictionaries methods

--- Data Structure/dictionary.py
+++ Data Structure/dictionary.py
@@ -206,3 +206,114 @@ del thisdict
 print(thisdict) #this will cause an error because "thisdict" no longer exists.
 
 #  ********************************************************* #
+
+#  ********************************************************* #
+
+# Loop Dictionaries
+
+# Print all key names in the dictionary, one by one:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+
+# Print all values in the dictionary, one by one:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(thisdict[x])
+
+#You can also use the values() method to return values of a dictionary:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.values():
+  print(x)
+
+# Loop through both keys and values, by using the items() method:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x, y in thisdict.items():
+  print(x, y)
+
+#  ********************************************************* #
+
+#  ********************************************************* #
+# Copy Dictionaries
+
+# Make a copy of a dictionary with the copy() method:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+
+# Make a copy of a dictionary with the dict() function:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+#  ********************************************************* #
+
+#  ********************************************************* #
+# Nested Dictionaries
+
+# Example
+# Create a dictionary that contain three dictionaries:
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(myfamily)
+
+# Example
+# Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+print(myfamily)
+
+#  ********************************************************* #
