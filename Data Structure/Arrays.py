15d770c51aa47f961ad32979bd810748a898f810
osaidAthar <osaidathar786633270@gmail.com>
2022-08-08 22:59:49 +0530

    classes and object methods

--- Data Structure/Arrays.py
+++ Data Structure/Arrays.py
@@ -4,4 +4,41 @@
 
 cars = ["Ford", "Volvo", "BMW"]
 print(cars)
+
+# Get the value of the first array item:
+x = cars[0]
+print(x)
+
+# Modify the value of the first array item:
+cars[0] = "Toyota"
+print(cars)
+
+# The Length of an Array
+
+# Return the number of elements in the cars array:
+x = len(cars)
+print(x)
+
+# Looping Array Elements
+
+# Print each item in the cars array:
+for x in cars:
+  print(x)
+
+# Adding Array Elements
+
+# Add one more element to the cars array:
+cars = ["Ford", "Volvo", "BMW"]
+cars.append("Honda")
+print(cars)
+
+# Removing Array Elements
+
+# Delete the second element of the cars array:
+cars = ["Ford", "Volvo", "BMW"]
+cars.pop(1)
+print(cars)
+
+
+
 #  ********************************************************* #
