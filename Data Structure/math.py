5d64a0aa174df6f738f34103a6d2ad941836d828
osaidAthar <osaidathar786633270@gmail.com>
2022-08-09 23:11:31 +0530

    maths methods

---
+++ Data Structure/math.py
@@ -0,0 +1,40 @@
+# ********************************************************* #
+# Maths
+
+# Built-in Math Functions
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+
+# The abs() function returns the absolute (positive) value of the specified number:
+x = abs(-7.25)
+print(x)
+
+# The pow(x, y) function returns the value of x to the power of y (xy).
+
+# Return the value of 4 to the power of 3 (same as 4 * 4 * 4):
+x = pow(4, 3)
+print(x)
+
+#  The Math Module
+
+# The math.sqrt() method for example, returns the square root of a number:
+import math
+x = math.sqrt(64)
+print(x)
+
+# The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number downwards to its nearest integer, and returns the result:
+
+import math
+x = math.ceil(1.4)
+y = math.floor(1.4)
+print(x) # returns 2
+print(y) # returns 1
+
+# The math.pi constant, returns the value of PI (3.14...):
+import math
+x = math.pi
+print(x)
+
+# ********************************************************* #
