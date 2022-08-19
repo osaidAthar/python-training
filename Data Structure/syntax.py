7c015d7944774df38e654e4030b2eb8250e1a54d
osaidAthar <osaidathar786633270@gmail.com>
2022-08-19 20:53:46 +0530

    syntax methods

---
+++ Data Structure/syntax.py
@@ -0,0 +1,31 @@
+# ********************************************************* #
+# Syntax
+
+# Python Indentation
+if 5 > 2:
+  print("Five is greater than two!")
+
+# The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+
+# Python Variables
+
+# Variables in Python:
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+# Comments
+
+# Comments in Python:
+#This is a comment.
+print("Hello, World!")
+
+
+# ********************************************************* #
