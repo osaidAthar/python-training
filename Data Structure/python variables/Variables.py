f9486e2f73551ed37e6bd6867f838dce04a71073
osaidAthar <osaidathar786633270@gmail.com>
2022-08-23 21:33:03 +0530

    variables methods

---
+++ Data Structure/python variables/Variables.py
@@ -0,0 +1,31 @@
+# ********************************************************* #
+# Creating Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# Casting
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+# Single or Double Quotes?
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
+
+# Case-Sensitive
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
+
+# ********************************************************* #
