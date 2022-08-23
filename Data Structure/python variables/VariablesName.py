2a7704a2521057288ec2c31c3d19886bb4b8ab5d
osaidAthar <osaidathar786633270@gmail.com>
2022-08-23 21:41:03 +0530

    variables methodes

---
+++ Data Structure/python variables/VariablesName.py
@@ -0,0 +1,41 @@
+# ********************************************************* #
+# Variable Names
+
+# Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+# Illegal variable names:
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+#This example will produce an error in the result
+
+# Multi Words Variable Names
+
+# Camel Case
+myVariableName = "John"
+
+# Pascal Case
+
+# Each word starts with a capital letter:
+MyVariableName = "John"
+
+# Snake Case
+
+# Each word is separated by an underscore character:
+my_variable_name = "John"
+
+# ********************************************************* #
