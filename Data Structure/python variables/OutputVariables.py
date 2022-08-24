818ceb7c14ce7ab1814c4f41b56243dbc1003fb2
osaidAthar <osaidathar786633270@gmail.com>
2022-08-24 22:48:38 +0530

    OutputVariables methods

---
+++ Data Structure/python variables/OutputVariables.py
@@ -0,0 +1,33 @@
+# ********************************************************* #
+# Output Variables
+x = "Python is awesome"
+print(x)
+
+# In the print() function, you output multiple variables, separated by a comma:
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# You can also use the + operator to output multiple variables:
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+# For numbers, the + character works as a mathematical operator:
+x = 5
+y = 10
+print(x + y)
+
+# In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error:
+x = 5
+y = "John"
+print(x + y)
+
+# The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types:
+x = 5
+y = "John"
+print(x, y)
+
+# ********************************************************* #
