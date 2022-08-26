eb6265abe65aeac8a9a21ce4c6ed3ed8614397f7
osaidAthar <osaidathar786633270@gmail.com>
2022-08-26 21:02:06 +0530

    dataTypes methods

---
+++ Data Structure/DataTypes.py
@@ -0,0 +1,119 @@
+# ********************************************************* #
+# data types
+
+# Print the data type of the variable x:
+x = 5
+print(type(x))
+
+# Setting the Data Type
+# Example                                    # Data Type      
+# x = "Hello World"       	                    str  
+# x = 20                                        int
+# x = 20.5                                      float
+# x = 1j                                        complex
+# x = ["apple", "banana", "cherry"]             list
+# x = ("apple", "banana", "cherry")             tuple
+# x = range(6)                                  range
+# x = {"name" : "John", "age" : 36}             dict  
+# x = {"apple", "banana", "cherry"}             set  
+# x = frozenset({"apple", "banana", "cherry"})  frozenset
+# x = True                                      bool    
+# x = b"Hello"                                  bytes  
+# x = bytearray(5)                              bytearray     
+# x = memoryview(bytes(5))                      memoryview     
+# x = None                                      NoneType    
+
+
+x = "Hello World"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = 20
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = 20.5
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = 1j
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = ["apple", "banana", "cherry"]
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = ("apple", "banana", "cherry")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = range(6)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = {"name" : "John", "age" : 36}
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = {"apple", "banana", "cherry"}
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = frozenset({"apple", "banana", "cherry"})
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = True
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = b"Hello"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = bytearray(5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = memoryview(bytes(5))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = None
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+
+# ********************************************************* #
