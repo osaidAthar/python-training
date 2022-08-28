6cfa4bcb5a880f48f64253d8d8b1755a0b69296d
osaidAthar <osaidathar786633270@gmail.com>
2022-08-28 18:32:39 +0530

    stringmethods

---
+++ Data Structure/python Strings/SlicingStrings.py
@@ -0,0 +1,27 @@
+# ********************************************************* #
+# Slicing
+
+# Get the characters from position 2 to position 5 (not included):
+b = "Hello, World!"
+print(b[2:5])
+
+# Slice From the Start
+
+# Get the characters from the start to position 5 (not included):
+b = "Hello, World!"
+print(b[:5])
+
+# Slice To the End
+
+# Get the characters from position 2, and all the way to the end:
+b = "Hello, World!"
+print(b[2:])
+
+# Negative Indexing
+
+# From: "o" in "World!" (position -5)
+# To, but not included: "d" in "World!" (position -2):
+b = "Hello, World!"
+print(b[-5:-2])
+
+# ********************************************************* #
