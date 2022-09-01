1245b62f16703d2401c9c9b26ebcd9e585f307ef
osaidAthar <osaidathar786633270@gmail.com>
2022-09-01 22:32:49 +0530

    EscapeCharacters methods

---
+++ Data Structure/python Strings/EscapeCharacters.py
@@ -0,0 +1,42 @@
+# ********************************************************* #
+# Escape Characters
+
+# Code                   Result
+# \'	                 Single Quote	
+# \\	                 Backslash	
+# \n	                 New Line	
+# \r	                 Carriage Return	
+# \t	                 Tab	
+# \b	                 Backspace	
+# \f	                 Form Feed	
+# \ooo	                 Octal value	
+# \xhh	                 Hex value
+
+txt = 'It\'s alright.'
+print(txt) 
+
+txt = "This will insert one \\ (backslash)."
+print(txt) 
+
+txt = "Hello\nWorld!"
+print(txt) 
+
+txt = "Hello\rWorld!"
+print(txt) 
+
+txt = "Hello\tWorld!"
+print(txt) 
+
+#This example erases one character (backspace):
+txt = "Hello \bWorld!"
+print(txt) 
+
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt) 
+
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
+
+# ********************************************************* #
