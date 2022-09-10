0a76e75e794fe3f93ba2fa5995a035eff5ca0c61
osaidAthar <osaidathar786633270@gmail.com>
2022-09-10 22:25:13 +0530

    FileHandling methods

---
+++ Data Structure/FileHandling.py
@@ -0,0 +1,12 @@
+# ********************************************************* #
+# File Handling
+
+# To open a file for reading it is enough to specify the name of the file:
+
+f = open("demofile.txt")
+
+# The code above is the same as:
+
+f = open("demofile.txt", "rt")
+
+# ********************************************************* #
